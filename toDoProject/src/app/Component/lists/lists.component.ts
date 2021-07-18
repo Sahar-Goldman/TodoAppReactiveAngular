803f30ecb3c8e42a1c364d536e0ad95ca7344d97
Sahar-Gold <63187916+Sahar-Gold@users.noreply.github.com>
2021-07-18 17:22:45 +0300

    added implementaion for lists page

--- toDoProject/src/app/Component/lists/lists.component.ts
+++ toDoProject/src/app/Component/lists/lists.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { List } from 'src/app/core/models/list.model';
+import { StateService } from 'src/app/core/services/state.service';
 
 @Component({
   selector: 'app-lists',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  lists$!: Observable<List[]>;
 
-  constructor() { }
+  constructor(
+    private state: StateService, 
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.lists$ = this.state.getAllLists();
+  }
+
+  navigateToList(id: number) {
+    this.router.navigate(['lists', id]);
+  }
+
+  navigateToNew() {
+    this.router.navigate(['lists', -1, 'edit']);
   }
 
 }

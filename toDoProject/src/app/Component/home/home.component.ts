2dc73de1a55d0f6da678096cf49dafefe5024358
Sahar-Gold <63187916+Sahar-Gold@users.noreply.github.com>
2021-07-18 17:04:52 +0300

    added home component

--- toDoProject/src/app/Component/home/home.component.ts
+++ toDoProject/src/app/Component/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { StateService } from 'src/app/core/services/state.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  today = Date.now();
+  developer = 'Sahar Goldman';
+  application = 'Todos app';
+  itemsCount$!: Observable<number>;
+  uncompletedCount$!: Observable<number>;
+  listsCount$!: Observable<number>;
 
-  constructor() { }
+  constructor(
+    private state: StateService, 
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.itemsCount$ = this.state.getAllItems().pipe(
+      map(items => items.length)
+    );
+    this.uncompletedCount$ = this.state.getAllItems().pipe(
+      map(items => items.filter(i => !i.isCompleted).length)
+    );
+    this.listsCount$ = this.state.getAllLists().pipe(
+      map(items => items.length)
+    )
+
+  }
+
+  navigateToNew() {
+    this.router.navigate(['lists', -1, 'edit']);
+  }
+
+  navigateToLists() {
+    this.router.navigate(['lists']);
+  }
+
+  navigateToItems() {
+    this.router.navigate(['items']);
   }
 
 }

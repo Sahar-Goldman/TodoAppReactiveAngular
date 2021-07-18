51a82db0a69c55ec5e38b25d55cc71aeb8501b70
Sahar-Gold <63187916+Sahar-Gold@users.noreply.github.com>
2021-07-18 15:04:14 +0300

    created inner components and guard case lists empty

--- toDoProject/src/app/app-routing.module.ts
+++ toDoProject/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Component/home/home.component';
 import { ItemsComponent } from './Component/items/items.component';
+import { ListEditorComponent } from './Component/list-editor/list-editor.component';
+import { ListViewerComponent } from './Component/list-viewer/list-viewer.component';
 import { ListsComponent } from './Component/lists/lists.component';
 import { NotFoundComponent } from './Component/not-found/not-found.component';
 
@@ -9,6 +11,8 @@ const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent}, 
   {path: 'lists', component: ListsComponent}, 
+  {path: 'lists/:id', component: ListViewerComponent}, 
+  {path: 'lists/:id/edit', component: ListEditorComponent}, 
   {path: 'items', component: ItemsComponent}, 
   {path: '**', component: NotFoundComponent}
 ];
